a38ffc0b4f87e9ada524183a071c2fdcc6c3239d
Maneth Pathirana <61646614+SmpManeth@users.noreply.github.com>
2023-01-02 09:32:37 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
+import PopUp from './Components/PopCustomer/PopUp';
+// import LoginPage from './Pages/LoginPage/index';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LoginPage/> */}
+      <PopUp />
+    </div>
+  );
+}
+
+export default App;
